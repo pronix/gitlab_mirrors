2096fdeca8632f5bd945b605b985e9a1160fafb0
dima <pronix.service@gmail.com>
2015-06-24 11:23:06 +0200

    fix proxy

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,11 @@
 class gitlab_mirrors::install{
   # this class requires pip and python to be installed
-  package{'gitlab3':
-    ensure => '0.5.4',
-    provider => 'pip',
+  #package{'gitlab3':
+  #  ensure => '0.5.4',
+  #  provider => 'pip',
+  #}
+  exec {"install gitlab3":
+    command => '/usr/bin/pip install gitlab3'
   }
 
 }
